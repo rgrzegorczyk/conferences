2f89613014121c0db3f7d3807f7eb5f272a75bc7
Rafa Grzegorczyk <rgrzegorczyk@pretius.com>
2026-04-10 14:03:48 +0200

    APEX_ALPE_ADRIA_Oracle SQLcl_The_Free_Tool_Every_PL_SQL_and_APEX_Developer_Needs - fixed slides + added mcp restrict levels

---
+++ 2026/Oracle SQLcl The Free Tool Every PL SQL and APEX Developer Needs/APEX_ALPE_ADRIA_2026/enjoy_apex_alpe_adria.sql
@@ -0,0 +1,33 @@
+SET FEEDBACK OFF
+SET TERMOUT ON
+
+PROMPT +============================================================+
+PROMPT |                                                            |
+PROMPT |           ENJOY THE REST OF THE CONFERENCE!                |
+PROMPT |                                                            |
+PROMPT +============================================================+
+exec dbms_session.sleep(2);
+PROMPT |                                                            |
+PROMPT |  A huge thank you to all the attendees for your time       |
+PROMPT |  and for choosing to listen to me today.                   |
+exec dbms_session.sleep(2);
+PROMPT |                                                            |
+PROMPT +------------------------------------------------------------+
+PROMPT |  >> RECOMMENDED SESSION BY MY COLLEAGUE from PRETIUS:      |
+exec dbms_session.sleep(2);
+PROMPT |                                                            |
+PROMPT |  APEX will make you sweat!                                 |
+PROMPT |  A job well done or embarassment - good and bad            |
+PROMPT |  practices in APEX!                                        |
+exec dbms_session.sleep(2);
+PROMPT |                                                            |
+PROMPT |  Speaker: Dominik Grabiński                                |
+PROMPT |  Date:    Today                                            |
+PROMPT |  Time:    15:35 - 16:20                                    |
+PROMPT |  Room:    D                                                |
+PROMPT |                                                            |
+exec dbms_session.sleep(2);
+PROMPT |  Thank you! Feel free to reach me at                       |
+PROMPT |  the event or online!                                      |
+PROMPT +============================================================+
+PROMPT
