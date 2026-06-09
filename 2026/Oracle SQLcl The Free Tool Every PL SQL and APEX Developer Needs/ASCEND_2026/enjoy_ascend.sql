3cf941a597266421ec24c13ea6fbccfde04d569e
Rafa Grzegorczyk <rgrzegorczyk@pretius.com>
2026-06-08 22:24:09 -0400

    Latest PDF version for ASCEND 2026 Orlando conference

---
+++ 2026/Oracle SQLcl The Free Tool Every PL SQL and APEX Developer Needs/ASCEND_2026/enjoy_ascend.sql
@@ -0,0 +1,17 @@
+SET FEEDBACK OFF
+SET TERMOUT ON
+
+PROMPT +============================================================+
+PROMPT |                                                            |
+PROMPT |           ENJOY THE REST OF THE CONFERENCE!                |
+PROMPT |                                                            |
+PROMPT +============================================================+
+exec dbms_session.sleep(2);
+PROMPT |                                                            |
+PROMPT |  A huge thank you to all the attendees for your time       |
+PROMPT |  and for choosing to listen to me today.                   |
+exec dbms_session.sleep(2);
+PROMPT |  Thank you! Feel free to reach me at                       |
+PROMPT |  the event or online!                                      |
+PROMPT +============================================================+
+PROMPT
